57b31737b55a45b39ff17ea077d217718c9609bf
mrpochinowell20 <lalaayu20@gmail.com>
2022-06-27 09:18:14 +0700

    seederCompany

--- database/seeders/UserCompanySeeder.php
+++ database/seeders/UserCompanySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Companyy;
+use Faker\Provider\ar_EG\Company;
 use Illuminate\Database\Seeder;
 
 class UserCompanySeeder extends Seeder
@@ -14,22 +15,25 @@ class UserCompanySeeder extends Seeder
      */
     public function run()
     {
-        $companyy = new UserCompanySeeder();
-        $companyy->nama = "address";
-        $companyy->content = "Jl. Anggajaya 2 No.105, Sanggrahan, Condongcatur, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281";
+        $company = new Companyy();
+        $company->nama = "address";
+        $company->content = "Jl. Anggajaya 2 No.105, Sanggrahan, Condongcatur, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281";
+        $company->save();
 
-        $companyy = new UserCompanySeeder();
-        $companyy->nama = "nomor whatsapp";
-        $companyy->content = "0811-2643-949";
+        $company = new Companyy();
+        $company->nama = "nomor whatsapp";
+        $company->content = "0811-2643-949";
+        $company->save();
 
-        $companyy = new UserCompanySeeder();
-        $companyy->nama = "nomor handphone";
-        $companyy->content = "0811-3187-055";
+        $company = new Companyy();
+        $company->nama = "nomor handphone";
+        $company->content = "0811-3187-055";
+        $company->save();
 
-        $companyy = new UserCompanySeeder();
-        $companyy->nama = "description 1";
-        $companyy->content = "PT Widya Informasi Nusantara atau Widya Wicara merupakan bagian dari Widya Indonesia Group yang didirikan pada tanggal 26 Februari 2019. Kami menciptakan solusi untuk berbagai kebutuhan masyarakat Indonesia terkait dengan pengolahan ucapan atau bahasa. Kami telah menciptakan empat inovasi produk berbasis teknologi wicara: Smart Speakers Widya Wicara, Text to Speech Widya Wicara, Speech to Text Widya Wicara, dan Chatbot.";
-
+        $company = new Companyy();
+        $company->nama = "description 1";
+        $company->content = "PT Widya Informasi Nusantara atau Widya Wicara merupakan bagian dari Widya Indonesia Group yang didirikan pada tanggal 26 Februari 2019. Kami menciptakan solusi untuk berbagai kebutuhan masyarakat Indonesia terkait dengan pengolahan ucapan atau bahasa. Kami telah menciptakan empat inovasi produk berbasis teknologi wicara: Smart Speakers Widya Wicara, Text to Speech Widya Wicara, Speech to Text Widya Wicara, dan Chatbot.";
+        $company->save();
 
     }
 }
